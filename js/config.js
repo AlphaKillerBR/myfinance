7429f37ac67b425b0e16398fc60f0659dc273a8f
AlphaKillerBR <caiotlemos2000@gmail.com>
2026-07-02 14:56:46 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,5 +8,5 @@
  * só o botão "Conectar Google Drive" fica desativado.
  */
 const CONFIG = {
-  GOOGLE_CLIENT_ID: 'SEU_CLIENT_ID_AQUI',
+  GOOGLE_CLIENT_ID: '864971609751-o5k3h7h4paof0f5qtpcmrd16imco6ggt.apps.googleusercontent.com',
 };
